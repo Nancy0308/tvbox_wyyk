bbe2b92735905a7f5540188faf3ac66aa1e2b08e
aa123jg <159424854+aa123jg@users.noreply.github.com>
2024-04-27 21:31:00 +0800

    更新 310直播.js

--- js/310直播.js
+++ js/310直播.js
@@ -1,11 +1,11 @@
 var rule = {
-    title:'310鐩存挱',
+    title:'310直播',
     host:'http://www.310.tv',
     url:'/?s=0&t=1&a=fyclass&g=fypage',
     searchUrl:'',
     searchable:0,
     quickSearch:0,
-    class_name:'鐑棬&瓒崇悆&绡悆',
+    class_name:'热门&足球&篮球',
     class_url:'0&1&2',
     headers:{
         'User-Agent':'MOBILE_UA'
@@ -15,8 +15,8 @@ var rule = {
     lazy:'',
     limit:6,
     double:false,
-    鎺ㄨ崘:'*',
-    涓€绾 :'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
-    浜岀骇:'*', 
-    鎼滅储:'',
+    推荐:'*',
+    一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
+    二级:'*', 
+    搜索:'',
 }
